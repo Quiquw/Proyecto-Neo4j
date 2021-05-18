ca0fae28feb3cb4a3aa711b452bbcb15e8a195e8
Salvador Enrique Alfaro Anaya <salvador.alfaro@alumnos.uneatlantico.es>
2021-05-18 14:00:24 +0200

    Added Front

--- app.js
+++ app.js
@@ -17,73 +17,114 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'SEAA'));
 var session = driver.session();
+var session2 = driver.session();
+var session3 = driver.session();
+var session4 = driver.session();
 
-app.get('/', function(req,res){
+app.get('/', function(req, res) {
     res.render('index');
 })
 
-app.get('/teams', function(req, res){
+app.get('/teams', function(req, res) {
     session
         .run('MATCH(n:Team) RETURN n LIMIT 25')
-        .then(function(result){
+        .then(function(result) {
             var teamArr = [];
-            result.records.forEach(function(record){
+            result.records.forEach(function(record) {
                 teamArr.push({
                     id: record._fields[0].identity.low,
                     name: record._fields[0].properties.name,
                     base: record._fields[0].properties.base,
-                    championships: record._fields[0].properties.championships
+                    championships: record._fields[0].properties.championships,
+                    image: record._fields[0].properties.image
                 });
             });
-           res.render('teams', {
-               teams: teamArr
-           });
+            res.render('teams', {
+                teams: teamArr
+            });
         })
-        .catch(function(err){
+        .catch(function(err) {
             console.log(err);
         });
 });
 
-app.get('/pilots', function(req, res){
+app.get('/pilots', function(req, res) {
     session
-        .run('MATCH(n:Pilot) RETURN n LIMIT 25')
-        .then(function(result){
+        .run('MATCH(n:Pilot) RETURN n LIMIT 50')
+        .then(function(result) {
             var pilotArr = [];
-            result.records.forEach(function(record){
+            result.records.forEach(function(record) {
                 pilotArr.push({
                     id: record._fields[0].identity.low,
                     name: record._fields[0].properties.name,
                     number: record._fields[0].properties.number,
-                    country: record._fields[0].properties.country
-
+                    country: record._fields[0].properties.country,
+                    image: record._fields[0].properties.image
                 });
             });
-           res.render('pilots', {
-               pilots: pilotArr
-           });
+            res.render('pilots', {
+                pilots: pilotArr
+            });
         })
-        .catch(function(err){
+        .catch(function(err) {
             console.log(err);
         });
 });
 
-app.post('/teams/add', function(req,res){
+app.post('/teams/add', function(req, res) {
     var name = req.body.name;
     var base = req.body.base;
     var championships = req.body.championships;
+    var image = req.body.image;
 
-    session
-        .run('CREATE(n:Team {name:{nameParam}, {base:{baseParam}, {championships:{championshipsParam}}) RETURN n.name', {nameParam:name, baseParam:base, championshipsParam:championships})
-        .then(function(result){
-            res.redirect('/');
-            session.close();
+    session2
+        .run('CREATE(n:Team {name: $nameParam, base: $baseParam, championships: $championshipsParam, image:$imageParam}) RETURN n.name', { nameParam: name, baseParam: base, championshipsParam: championships, imageParam: image })
+        .then(function(result) {
+            res.setHeader('Acces-Control-Allow-Origin', '*');
+            session2.close();
         })
-        .catch(function(err){
+        .catch(function(err) {
             console.log(err);
         });
 
-    
-    res.redirect('/');
+    res.redirect('/teams');
+
+});
+
+app.post('/pilots/add', function(req, res) {
+    var name = req.body.name;
+    var number = req.body.number;
+    var country = req.body.country;
+    var image = req.body.image;
+
+    session3
+        .run('CREATE(n:Pilot {name: $nameParam, number: $numberParam, country: $countryParam, image: $imageParam}) RETURN n.name', { nameParam: name, numberParam: number, countryParam: country, imageParam: image })
+        .then(function(result) {
+            //res.setHeader('Acces-Control-Allow-Origin', '*');
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+
+    res.redirect('/pilots');
+
+});
+
+app.post('/pilots/team/add', function(req, res) {
+    var team = req.body.team;
+    var pilot = req.body.pilot;
+
+    session4
+        .run('MATCH (a:Pilot {name:$pilotParam}), (b:Team {name:$teamParam}) MERGE(a)-[r:DRIVES_FOR]-(b) RETURN a,b', { pilotParam: pilot, teamParam: team })
+        .then(function(result) {
+            //res.setHeader('Acces-Control-Allow-Origin', '*');
+            session4.close();
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+
+    res.redirect('/pilots');
 
 });
 
